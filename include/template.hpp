35832067d5a8918f4505988fcb32e3a98975c45d
carykees98 <trainboy980@gmail.com>
2024-06-27 17:50:47 -0400

    updated clang-tidy
    Signed-off-by: carykees98 <trainboy980@gmail.com>

---
+++ include/template.hpp
@@ -0,0 +1,13 @@
+#pragma once
+/**
+ * @file template.hpp
+ * @author Cary Keesler
+ * @brief 
+ * @version 0.1.0
+ * @date 2024-06-24
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+auto example_func() -> int;

9f7b0177823b7fbe530686ce20bad867d8b4fe9c
carykees98 <trainboy980@gmail.com>
2024-06-27 20:35:28 -0400

    template main and library files

---
+++ core/main.cpp
@@ -0,0 +1,26 @@
+/**
+ * @file main.cpp
+ * @author Cary Keesler
+ * @brief Example main file
+ * @version 0.1.0
+ * @date 2024-06-24
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#include <format>
+#include <iostream>
+
+#include <template.hpp>
+
+/**
+ * @brief Entry point for program
+ * 
+ * @return Status code of the program
+ */
+auto main() -> int 
+{
+    std::cout << std::format("Hello {}!", "World") << std::endl;
+    return example_func();
+}
---
+++ src/template.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file template.cpp
+ * @author Cary Keesler
+ * @brief 
+ * @version 0.1.0
+ * @date 2024-06-24
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#include <template.hpp>
+
+auto example_func() -> int {
+    return 5;
+}
